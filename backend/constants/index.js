b3394a30673890d88d271c35326575161c715272
Mryesiller <gokselyesiller@gmail.com>
2022-05-25 13:05:06 +0300

    new contract address deployed

--- backend/constants/index.js
+++ backend/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x8deC23F063821D7B72eF8aE45b4c0C0BF80344a2";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "solidity-nft-collection-la9ff3jq4-mryesiller.vercel.app/api/";
+const METADATA_URL = "https://solidity-nft-collection.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
